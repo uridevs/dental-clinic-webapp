097ea5aa797371207fc4412ac8b5b5ec4886b7da
Javi <66311117+uridevs@users.noreply.github.com>
2024-05-10 21:02:52 +0200

    Update Footer.jsx

--- cliente/dental-lux-app/src/layout/Footer.jsx
+++ cliente/dental-lux-app/src/layout/Footer.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
+<<<<<<< HEAD
+=======
             <hr />
             <h3>Footer</h3>
+>>>>>>> a5edc13b430c03700f3326f619581dfa5ec9c1ff
             <p>Copyright © Dental Luxe</p>
         </footer>
     );
